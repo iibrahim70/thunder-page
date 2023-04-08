8247fb494bf019f4b3f345edfaa8fe8b5378d901
iibrahim70 <aviknuhash70@gmail.com>
2023-04-09 00:32:35 +0600

    added navbar of the webpage

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import Books from './components/Books';
 
 const router = createBrowserRouter([
   {
     path: '/', 
     element: <App></App>,  
-  }
+    children: [
+      {
+        path: '/', 
+        element: <Home></Home>, 
+      },
+      {
+        path: 'books', 
+        element: <Books></Books>, 
+      },
+      {
+        path: 'about', 
+        element: <About></About>, 
+      },
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
